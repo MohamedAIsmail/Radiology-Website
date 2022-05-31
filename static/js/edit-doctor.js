165fe5e79fd4f95f57ab58861b335faa6ab72f66
MohabGhobashy <mohab.ali01@eng-st.cu.edu.eg>
2022-05-31 08:51:07 +0200

    medical info edit page

---
+++ static/js/edit-doctor.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">
+    document.getElementById("myButton").onclick = function () {
+        location.href = "https://stackoverflow.com/questions/16562577/how-can-i-make-a-button-redirect-my-page-to-another-page"};
+</script>
