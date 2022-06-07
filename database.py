fe0cc41f0ef16430a27865118064be557a13947b
Mo-Moustafa <momoustafa227@gmail.com>
2022-06-07 07:58:59 +0200

    simple front edits

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def recreatedb(bool):
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="root"
+        passwd="1234"
     )
 
     mycursor = mydb.cursor()
@@ -18,7 +18,7 @@ def recreatedb(bool):
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="root",
+        passwd="1234",
         database="Raddb"
     )
 
@@ -172,7 +172,7 @@ def connect():
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="root",
+        passwd="1234",
         database="Raddb"
     )
 
